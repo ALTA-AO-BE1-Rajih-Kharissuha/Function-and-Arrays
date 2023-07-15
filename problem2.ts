9cd14105875eb12618d9a980c2d64f5fb437581d
Rajihkharissuha12 <rajhk704@gmail.com>
2023-07-14 20:34:57 -0700

    Problem 2

---
+++ problem2.ts
@@ -0,0 +1,16 @@
+function joinArrayRemoveDuplicate(arrayA:String[], arrayB:String[]) {
+    // your code here
+    return arrayA.concat(arrayB)
+  }
+  // Test cases
+  console.log(joinArrayRemoveDuplicate(["apel", "anggur"], ["lemon", "leci", "nanas"]))
+  // ["apel", "anggur", "lemon", "leci", "nanas"]
+  
+  
+  console.log(joinArrayRemoveDuplicate(["samsung", "apple"], ["apple", "sony", "xiaomi"]))
+  // ["samsung", "apple", "sony", "xiaomi"]
+  
+  
+  console.log(joinArrayRemoveDuplicate(["football", "basketball"], ["basketball", "football"]))
+  // [“football”, “basketball”]
+  

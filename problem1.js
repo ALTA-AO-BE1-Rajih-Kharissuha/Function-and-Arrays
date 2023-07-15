eaa5b1d9cb21da6cda9d9a60ae55ccd2510e8934
Rajihkharissuha12 <rajhk704@gmail.com>
2023-07-14 19:38:03 -0700

    Problem1

---
+++ problem1.js
@@ -0,0 +1,16 @@
+function exponentiation(x, n) {
+  // your code here
+  if (x < 0 || n < 0) {
+    return "wrong input";
+  } else {
+    return x ** n;
+  }
+}
+
+console.log(exponentiation(2, 3)); // 8
+console.log(exponentiation(2, 12)); // 4096
+console.log(exponentiation(7, 2)); // 49
+console.log(exponentiation(9, 3)); // 729
+console.log(exponentiation(22, 5)); // 5153632
+console.log(exponentiation(1996, 0)); // 1
+console.log(exponentiation(4213, -3)); // “wrong input”
